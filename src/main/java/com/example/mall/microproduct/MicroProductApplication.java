c1b8559e507249a2a40dca2ad14fa921cfc43ee4
Xin Chen <xinszchen@thoughtworks.com>
2018-09-28 14:58:42 +0800

    init project

---
+++ src/main/java/com/example/mall/microproduct/MicroProductApplication.java
@@ -0,0 +1,12 @@
+package com.example.mall.microproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroProductApplication.class, args);
+    }
+}
